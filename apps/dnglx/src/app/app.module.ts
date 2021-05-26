a746fbbf1e117942e778dfddb0f2c4e269eae5a5
Risav Karna <risavkarna@gmail.com>
2021-05-26 18:16:47 +0545

    (update) add material dashboard setup/scaffolds

--- apps/dnglx/src/app/app.module.ts
+++ apps/dnglx/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeGridComponent } from './home-grid/home-grid.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, HomeGridComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
