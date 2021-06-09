9a28a0cbbd25fb42f431f279bc4488ddf1d44232
Risav Karna <risavkarna@gmail.com>
2021-06-09 20:40:47 +0545

    (update) add fastify, vagrant conf, tweaked containerization steps

--- nodes/containers/init.sh
+++ nodes/containers/init.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-# Get core base
+
+# Utility to stop and remove if existing
+stop_remove_safe() {
+  OLD="$(docker ps --all --quiet --filter=name="$1")"
+  if [ -n "$OLD" ]; then
+    echo "Removing existing container: $1 ..."
+    docker stop "$OLD" && docker rm "$OLD"
+  fi
+}
+
+echo "#0 Get core base's required images nginx-alpine and bitnami-couchdb"
+# 1. [Decorated] For Angular Federated Modules, usually with Svelte/Plain TypeScript/Rescript
 docker pull nginx:1.21-alpine
+# 2. For UI component driven UIX-API, guaranteed to be the way the UI needs the API responses to be
 docker pull bitnami/couchdb:3.1.1
 
-# Get opt-core base
-docker pull timescale/timescaledb-postgis:latest-pg12
-docker pull bitnami/keycloak:13
-# docker pull postgrest/postgrest:v7.0.1
-docker pull bitnami/nginx:1.19
+echo "#1 Building and starting core base..."
 
-echo "Starting and building..."
-
-echo "# 1. Containerize and start dist server"
-docker stop dnglx 2> /dev/null
-docker rm -f dnglx 2> /dev/null
+NGX_FED_MODS="dnglx"
+echo "#1.1. Containerize and start nginxngx dist server: $NGX_FED_MODS"
+stop_remove_safe "$NGX_FED_MODS"
 docker build -t nginxngx-image:v1 .
-docker run -d --name dnglx -p 80:80 nginxngx-image:v1
+docker run -d --name "$NGX_FED_MODS" -p 80:80 nginxngx-image:v1
 
-echo "# 2. Set up couch-pot-ato"
-docker stop couch-pot-ato 2> /dev/null
-docker rm -f couch-pot-ato 2> /dev/null
-docker run -d --name couch-pot-ato \
+COUCH_ERL="couch-pot-ato"
+echo "#1.2. Set up couch-pot-ato server: $COUCH_ERL"
+stop_remove_safe "$COUCH_ERL"
+docker run -d --name "$COUCH_ERL" \
   -p 5984:5984 \
   -e COUCHDB_PASSWORD=password \
   -v couchdb:/bitnami/couchdb \
@@ -33,13 +39,28 @@ docker run -d --name couch-pot-ato \
 # COUCHDB_CLUSTER_PORT_NUMBER: Port for cluster communication. Default: 9100
 # COUCHDB_BIND_ADDRESS: Address binding for the standard port. Default: 0.0.0.0
 
-echo "# 3. Set up postgre-space-time"
-docker network create keycloak-network 2> /dev/null
-docker stop pg-space-time 2> /dev/null
-docker rm -f pg-space-time 2> /dev/null
-docker run -d --name pg-space-time \
+echo "#2. Get opt core's required images timescaledb-postgis, bitnami-keycloak and bitnami-nginx"
+
+# Get opt-core base
+# 3. [Linked 0] For Relational, Time series and GIS datasets
+docker pull timescale/timescaledb-postgis:latest-pg12
+# 4. [Linked 0] For OIDC/JWT and complex user roles, authorizations etc
+docker pull bitnami/keycloak:13
+# 5. [Decorated, Linked 0] Mini-deb for prox-e-g APIs (Ledgers, Dashboards, MACERs)
+docker pull bitnami/nginx:1.19
+
+echo "#3 Building and starting opt core..."
+
+echo "#3.1 Set up postgre-space-time"
+
+KEY_NET="key-net"
+
+PG_SPACE_TIME="pg-space-time"
+docker network create "$KEY_NET" || true
+stop_remove_safe "$PG_SPACE_TIME"
+docker run -d --name "$PG_SPACE_TIME" \
   -p 5432:5432 \
-  --net keycloak-network \
+  --net "$KEY_NET" \
   -e POSTGRES_USER=pgst \
   -e POSTGRES_DB=pgst \
   -e POSTGRES_PASSWORD=password \
@@ -49,13 +70,13 @@ docker run -d --name pg-space-time \
   -v pgdata:/var/lib/postgresql/data \
   timescale/timescaledb-postgis:latest-pg12
 
-echo "# 4. Set up keycloak"
-docker stop keycloak-pot 2> /dev/null
-docker rm -f keycloak-pot 2> /dev/null
-docker run -d --name keycloak-pot \
-  --net keycloak-network \
+KEY_POT="key-pot"
+echo "#3.2 Set up $KEY_POT with $PG_SPACE_TIME"
+stop_remove_safe "$KEY_POT"
+docker run -d --name "$KEY_POT" \
+  --net "$KEY_NET" \
   -p 8080:8080 \
-  -e KEYCLOAK_DATABASE_HOST=pg-space-time \
+  -e KEYCLOAK_DATABASE_HOST="$PG_SPACE_TIME" \
   -e KEYCLOAK_DATABASE_NAME=pgst \
   -e KEYCLOAK_DATABASE_USER=pgst \
   -e KEYCLOAK_DATABASE_PASSWORD=password \
