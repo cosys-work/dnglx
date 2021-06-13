3f9ecd97a45dff271609b5d41dc0d822e82328bd
Risav Karna <risavkarna@gmail.com>
2021-06-14 01:18:12 +0545

    (update) add sec/auth nestjs packages, update main.ts setup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,6 +10,9 @@ import { AppModule } from './app/app.module';
 import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
 
 import compression from 'fastify-compress';
+import { fastifyHelmet } from 'fastify-helmet';
+import fastifyCsrf from 'fastify-csrf';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -19,10 +22,25 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('PROX E|G')
+    .setDescription('The prox-e|g APIs as described on risav.dev')
+    .setVersion('1.0')
+    .addTag('proxies')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, document);
+
+  await app.register(fastifyCsrf);
+  await app.register(compression, { encodings: ['gzip', 'deflate'] });
+  await app.register(fastifyHelmet);
+
+
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
-  await app.register(compression, { encodings: ['gzip', 'deflate'] });
 }
 
 bootstrap();
